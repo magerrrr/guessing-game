673c03d58b5b946ed8c70551078b168adfb23717
Kirill Mager <kiruha24@mail.ru>
2019-10-10 21:54:26 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.half = Math.round((this.min + this.max)/2);
+        return this.half;
     }
 
     lower() {
-
+        this.max = this.half;
     }
 
     greater() {
-
+        this.min = this.half;
     }
 }
 
